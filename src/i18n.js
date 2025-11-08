45c07d8140bd59274a84715ce0d2b50bfe66162c
olgapotapova <olga6263@inbox.ru>
2025-11-08 02:14:45 +0200

    add again languageDetector

--- src/i18n.js
+++ src/i18n.js
@@ -1,10 +1,10 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-// import LanguageDetector from 'i18next-browser-languagedetector';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
 
 i18n
-  // .use(LanguageDetector)
+  .use(LanguageDetector)
   .use(Backend) // add backend
   .use(initReactI18next)
   .init({
